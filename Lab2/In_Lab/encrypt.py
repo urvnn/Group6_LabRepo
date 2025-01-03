7c7a42d042f8e0979e2a9de02a31f59caa366d82
Arvin <169927030+urvnn@users.noreply.github.com>
2025-01-03 20:36:21 +0800

    Update encrypt.py

--- Lab2/In_Lab/encrypt.py
+++ Lab2/In_Lab/encrypt.py
@@ -1,3 +1,10 @@
+""" 
+  Fundamentals of Python: First Programs Chapter 04
+  Programming Exercises
+  
+  Write a script in the file encrypt.py that inputs a line of plaintext and a distance value and outputs an encrypted text using a Caesar cipher. 
+  The script should work for any printable characters. (LO: 4.1, 4.2)
+"""
 import os
 
 def clean():
