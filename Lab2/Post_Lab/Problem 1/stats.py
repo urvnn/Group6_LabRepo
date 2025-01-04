c7a0335481935ad8e7bca16f5e0611b4f941300d
ampnagal <163994417+ampnagal@users.noreply.github.com>
2025-01-04 08:07:19 +0000

    Changes to Post Problem 1

--- Lab2/Post_Lab/Problem 1/stats.py
+++ Lab2/Post_Lab/Problem 1/stats.py
@@ -1,15 +1,11 @@
 def mode(f):
-    # Input the text, convert its words to uppercase, and
-    # add the words to a list
+    
     words = []
     for line in f:
         wordsInLine = line.split()
         for word in wordsInLine:
             words.append(word.upper())
-
-    # Obtain the set of unique words and their
-    # frequencies, saving these associations in
-    # a dictionary
+    
     theDictionary = {}
     
     for word in words:
@@ -21,24 +17,19 @@ def mode(f):
             # word already seen, increment its number
             theDictionary[word] = number + 1
 
-    # Find the mode by obtaining the maximum value
-    # in the dictionary and determining its key
     theMaximum = max(theDictionary.values())
     for key in theDictionary:
         if theDictionary[key] == theMaximum:
             return key
 
-
 def median(f):
-    # Input the text, convert it to numbers, and
-    # add the numbers to a list
+    
     numbers = []
     for line in f:
         words = line.split()
         for word in words:
             numbers.append(float(word))
 
-    # Sort the list and return the number at its midpoint
     numbers.sort()
     midpoint = len(numbers) // 2
     if len(numbers) % 2 == 1:
